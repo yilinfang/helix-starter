727c5bf2af3a9dfe148360a63734285277bf93b6
Yilin Fang <qzfyl98@outlook.com>
2025-03-09 23:23:16 -0400

    Add script to install lsps

---
+++ install-lsp-linux-amd64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash "lsps/bash-language-server_npm_install.sh"
+bash "lsps/marksman_linux_amd64_install.sh"
+bash "lsps/ruff_pip_install.sh"
+bash "lsps/shfmt_linux_amd64_install.sh"
+bash "lsps/taplo_npm_install.sh"
+bash "lsps/prettier_npm_install.sh"
+bash "lsps/pyright_npm_install.sh"
---
+++ install-lsp-macos-arm64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+bash "lsps/bash-language-server_npm_install.sh"
+bash "lsps/marksman_macos_universal_install.sh"
+bash "lsps/ruff_pip_install.sh"
+bash "lsps/shfmt_macos_arm64_install.sh"
+bash "lsps/taplo_npm_install.sh"
+bash "lsps/prettier_npm_install.sh"
+bash "lsps/pyright_npm_install.sh"
